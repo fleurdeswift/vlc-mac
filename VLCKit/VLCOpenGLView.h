353ed7c48118dc68fe3d9e55cd7d7796bdf78128
Fleur de Swift <fleurdeswift@gmail.com>
2015-07-28 13:33:24 -0700

    Moved VLCOpenGLView in it's own file.

---
+++ VLCKit/VLCOpenGLView.h
@@ -0,0 +1,13 @@
+//
+//  VLCOpenGLView.h
+//  VLCKit
+//
+//  Copyright © 2015 Fleur de Swift. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import "VLCIOSurface.h"
+
+@interface VLCOpenGLView : NSOpenGLView <VLCIOSurface>
+@end
